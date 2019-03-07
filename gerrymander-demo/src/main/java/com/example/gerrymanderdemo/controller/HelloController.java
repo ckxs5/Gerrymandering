695213550845b9b5148482feb109245ebb90f77a
Kaixuan Chen <32051171+ckxs5@users.noreply.github.com>
2019-03-06 22:29:29 -0500

    Leaflet
    Using Leaflet instead of Google Map

--- gerrymander-demo/src/main/java/com/example/gerrymanderdemo/controller/HelloController.java
+++ gerrymander-demo/src/main/java/com/example/gerrymanderdemo/controller/HelloController.java
@@ -1,22 +1,21 @@
 package com.example.gerrymanderdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 @Controller
 public class HelloController {
 
-    @ResponseBody
-    @RequestMapping("/hello")
-    public String Hello(){
-        return "Hello World";
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
-
-    @ResponseBody
-    @RequestMapping("/hello/nihao")
-    public String nihao(){
-        return "Ni Hao";
-    }
-
 }
--- gerrymander-demo/src/main/webapp/index.js
+++ gerrymander-demo/src/main/webapp/index.js
@@ -1,17 +1,17 @@
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 44.985308, lng: -93.267210},
-        zoom: 6
-    });
-}
+console.log("asdfasdf");
+$("document").ready(function(){
+    function initalMap() {
+        console.log("asdfasdf");
+        var mymap = L.map('map').setView([44.985667, -93.267763], 13);
 
-function addpoly(){
-    polygon = map.drawPolygon({
-        paths: paths,
-        useGeoJSON: true,
-        strokeOpacity: 1,
-        strokeWeight: 3,
-        fillColor: '#BBD8E9',
-        fillOpacity: 0.6
-    });
-}
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            id: 'mapbox.streets'
+        }).addTo(mymap);
+    }
+
+    initalMap();
+});
