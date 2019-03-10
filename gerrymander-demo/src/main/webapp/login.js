10124a4d689324a52023fa972deb503d96b10463
Kaixuan Chen <32051171+ckxs5@users.noreply.github.com>
2019-03-10 15:04:55 -0400

    Login page map added

--- gerrymander-demo/src/main/webapp/login.js
+++ gerrymander-demo/src/main/webapp/login.js
@@ -1,9 +1,21 @@
 $(function () {
-    $(".sign-up-btn").click(function() {
+    $(".sign-up-btn").click(function () {
         $(".lightbox").delay(500).fadeIn(500);
     });
-    $(".btn-sm").click(function() {
+    $(".btn-sm").click(function () {
         $(this).stop().fadeOut();
+    });
+
+    $(".loginmap").ready(function () {
+        var mymap = L.map('loginmap').setView([44.985667, -93.267763], 13);
+
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            id: 'mapbox.streets'
+        }).addTo(mymap);
     });
 
 });
