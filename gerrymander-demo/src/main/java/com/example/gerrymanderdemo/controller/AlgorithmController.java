10e1954fd250c46f2286f249394885d2831f73dd
Kaixuan Chen <32051171+ckxs5@users.noreply.github.com>
2019-05-01 02:15:43 -0400

    Nothing changed

--- gerrymander-demo/src/main/java/com/example/gerrymanderdemo/controller/AlgorithmController.java
+++ gerrymander-demo/src/main/java/com/example/gerrymanderdemo/controller/AlgorithmController.java
@@ -1,4 +1,4 @@
-package com.example.gerrymanderdemo.Controller;
+package com.example.gerrymanderdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
--- gerrymander-demo/src/main/java/com/example/gerrymanderdemo/model/Data/Data.java
+++ gerrymander-demo/src/main/java/com/example/gerrymanderdemo/model/Data/Data.java
@@ -22,18 +22,19 @@ public class Data implements ResponseObject {
     @OneToOne
     Boundary boundary;
 
-    public Data(Data d1, Data d2) {
-        //TODO
+//    public Data(Data d1, Data d2) {
+//        //TODO
+//    }
+
+    //TODO
+    public Data(){
+
     }
 
     public Data(Vote voteData, Demographic demographic, Boundary boundary) {
         this.voteData = voteData;
         this.demographic = demographic;
         this.boundary = boundary;
-    }
-
-    public Data(Data a, Data b){
-
     }
 
     public Vote getVoteData() {
