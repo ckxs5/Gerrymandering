3157647f00cce7a2957b190fbc2efeb2800575fe
Kaixuan Chen <32051171+ckxs5@users.noreply.github.com>
2019-04-30 22:31:42 -0400

    passwordEncode

--- gerrymander-demo/src/main/java/com/example/gerrymanderdemo/Service/UserService.java
+++ gerrymander-demo/src/main/java/com/example/gerrymanderdemo/Service/UserService.java
@@ -1,10 +1,11 @@
 package com.example.gerrymanderdemo.Service;
 
-import com.example.gerrymanderdemo.PasswordEncoder.PasswordEncoder;
 import com.example.gerrymanderdemo.Repository.UserRepository;
 import com.example.gerrymanderdemo.model.User.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @Service
 public class UserService {
@@ -12,22 +13,21 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
 
     public User find(String userEmail, String password) {
         return userRepository.findByEmailAndPassword(userEmail, password);
     }
 
-    public User find(String userType){
-        return userRepository.findByUserType(userType);
-    }
 
     public User addUser(User user) {
         System.out.println("User: "+user);
         User add = userRepository.findByEmail(user.getEmail());
         System.out.println("Add: "+ add);
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+        System.out.println(hashedPassword);
+
         if (add != null) {
             return null;
         }else{
